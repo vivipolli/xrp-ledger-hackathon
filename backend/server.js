534b7b3f3e7064aa0f315aa6a173e7e3f6acc550
vivi <pollivivian@gmail.com>
2024-12-22 11:11:06 -0300

    fix port

--- backend/server.js
+++ backend/server.js
@@ -151,7 +151,7 @@ app.get("/nfts", async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
